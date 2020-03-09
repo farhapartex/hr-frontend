66049dad33207a4fc63b329f5f88aa7fbe637c9f
Md Nazmul Hasan <farhapartex@gmail.com>
2020-03-10 02:29:23 +0600

    login api integrated

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -18,15 +18,35 @@
             <h4 class="text-center">Login</h4>
           </div>
           <div class="auth-box mx-auto mt-3 mb-7">
+            <div
+              class="alert alert-danger text-center"
+              role="alert"
+              v-if="loginError==true"
+            >{{errorMessage}}</div>
             <div class="w-100">
               <div class="form-group">
-                <input type="text" class="form-control" placeholder="Username" />
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Username"
+                  v-model="user.username"
+                />
               </div>
               <div class="form-group">
-                <input type="password" class="form-control" placeholder="Password" />
+                <input
+                  type="password"
+                  class="form-control"
+                  placeholder="Password"
+                  v-model="user.password"
+                />
               </div>
               <div class="form-group">
-                <input type="submit" class="btn btn-sm btn-primary w-100" value="Login" />
+                <input
+                  type="submit"
+                  class="btn btn-sm btn-primary w-100"
+                  value="Login"
+                  @click="userLogin"
+                />
               </div>
             </div>
 
@@ -71,12 +91,58 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { Getter, Action } from "vuex-class";
 import HelloWorld from "@/components/HelloWorld.vue";
+import { AuthCredential } from "../store/store.types";
+import { LOGIN } from "../store/actions.names";
 
 @Component({
   name: "Auth",
   components: {}
 })
-export default class Auth extends Vue {}
+export default class Auth extends Vue {
+  @Action(LOGIN) login: any;
+
+  user: AuthCredential = {
+    username: "",
+    password: ""
+  };
+  errorMessage: string = "";
+  loginError: boolean = false;
+
+  setErrorMessege(message: string) {
+    this.loginError = true;
+    this.errorMessage = message;
+  }
+
+  removeErrorMessege() {
+    this.loginError = false;
+    this.errorMessage = "";
+  }
+
+  loginValidation() {
+    if (this.user == null) {
+      this.setErrorMessege("User credential can't be empty");
+      return false;
+    } else if (this.user.username == "" || this.user.password == "") {
+      this.setErrorMessege("User credential can't be empty");
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  userLogin() {
+    if (this.loginValidation()) {
+      this.login(this.user)
+        .then((result: any) => {
+          this.removeErrorMessege();
+          this.$router.push("/");
+        })
+        .catch((e: any) => {
+          this.setErrorMessege("Login Failed! Username/Password is wrong");
+        });
+    }
+  }
+}
 </script>
 
 <style>
